47801aa0e68ce24d2cb5a6adc50702360115e994
jiho <jihojihoya02@gmail.com>
2019-05-10 22:10:56 +0900

    글쓰기 화면 디자인

---
+++ app/src/main/java/com/codefair/lawfeedback/activity/WriteArticleActivity.java
@@ -0,0 +1,15 @@
+package com.codefair.lawfeedback.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.codefair.lawfeedback.R;
+
+public class WriteArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_article);
+    }
+}
