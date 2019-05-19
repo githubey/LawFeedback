15b1b9d1685ce9289541037ae8ed06d3ebd598b8
haeun <happyde0220@naver.com>
2019-05-20 00:27:11 +0900

    글 보기 DB값 채워넣기

--- app/src/main/java/com/codefair/lawfeedback/activity/ArticleViewActivity.java
+++ app/src/main/java/com/codefair/lawfeedback/activity/ArticleViewActivity.java
@@ -2,11 +2,16 @@ package com.codefair.lawfeedback.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.codefair.lawfeedback.R;
 import com.codefair.lawfeedback.listener.SuccessGettingArticleInfoListener;
 import com.codefair.lawfeedback.model.ArticleInfo;
+import com.codefair.lawfeedback.model.RelatedJobInfo;
 import com.codefair.lawfeedback.network.RetrofitManager;
+
+import java.util.List;
 
 import retrofit2.Response;
 
@@ -29,6 +34,38 @@ public class ArticleViewActivity extends AppCompatActivity implements SuccessGet
 
     @Override
     public void onOKResponse(Response<ArticleInfo> response) {
-        //TODO
+        TextView articleViewTitle = findViewById(R.id.articleViewTitle);
+        TextView articleViewContent = findViewById(R.id.articleViewContent);
+        TextView articleViewAllGoodText = findViewById(R.id.articleViewAllGoodText);
+        TextView articleViewAllBadText = findViewById(R.id.articleViewAllBadText);
+        TextView articleViewJob1Text = findViewById(R.id.articleViewJob1Text);
+        TextView articleViewJob1GoodText = findViewById(R.id.articleViewJob1GoodText);
+        TextView articleViewJob1BadText = findViewById(R.id.articleViewJob1BadText);
+        TextView articleViewJob2Text = findViewById(R.id.articleViewJob2Text);
+        TextView articleViewJob2GoodText = findViewById(R.id.articleViewJob2GoodText);
+        TextView articleViewJob2BadText = findViewById(R.id.articleViewJob2BadText);
+
+        ArticleInfo articleInfo = response.body();
+        assert articleInfo != null;
+        articleViewTitle.setText(articleInfo.getTitle());
+        articleViewContent.setText(articleInfo.getContent());
+        articleViewAllGoodText.setText("" + articleInfo.getGood_ex());
+        articleViewAllBadText.setText("" + articleInfo.getBad_ex());
+
+        List<RelatedJobInfo> relatedJobInfoList = articleInfo.getRelatedJobInfoList();
+        View articleViewJob1Layout = findViewById(R.id.articleViewJob1Layout);
+        View articleViewJob2Layout = findViewById(R.id.articleViewJob2Layout);
+        if (relatedJobInfoList != null && relatedJobInfoList.size() > 0) {
+            articleViewJob1Layout.setVisibility(View.VISIBLE);
+            articleViewJob1Text.setText(relatedJobInfoList.get(0).getJobName());
+            articleViewJob1GoodText.setText("" + relatedJobInfoList.get(0).getGood());
+            articleViewJob1BadText.setText("" + relatedJobInfoList.get(0).getBad());
+            if (relatedJobInfoList.size() > 1) {
+                articleViewJob2Layout.setVisibility(View.VISIBLE);
+                articleViewJob2Text.setText(relatedJobInfoList.get(1).getJobName());
+                articleViewJob2GoodText.setText("" + relatedJobInfoList.get(1).getGood());
+                articleViewJob2BadText.setText("" + relatedJobInfoList.get(1).getBad());
+            }
+        }
     }
 }
