c32fb10f088237dfbc85fa164c506c55bc2169dd
jiho <jihojihoya02@gmail.com>
2019-05-22 23:43:57 +0900

    앱 이름 변경

--- app/src/main/java/com/codefair/lawfeedback/listener/SuccessRegisterationLawmakerListListener.java
+++ app/src/main/java/com/codefair/lawfeedback/listener/SuccessRegisterationLawmakerListListener.java
@@ -1,11 +1,5 @@
 package com.codefair.lawfeedback.listener;
 
-import com.codefair.lawfeedback.model.Job;
-
-import java.util.List;
-
-import retrofit2.Response;
-
 public interface SuccessRegisterationLawmakerListListener {
     void onSuccessRegister();
 }
